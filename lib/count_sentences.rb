3454f1de6492c1da6fb1812aab075ead587a6735
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-02 18:56:37 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,7 +4,7 @@ class String
 
   def sentence
     self.end_with(".")
-        
+
   end
 
   def question?
@@ -12,10 +12,10 @@ class String
   end
 
   def exclamation?
-
+    self.end_with("!")
   end
 
   def count_sentences
-
+    
   end
 end
