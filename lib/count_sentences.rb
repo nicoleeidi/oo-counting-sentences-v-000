f577f7b9378ebf823f8edb901fa88ccc4f70103e
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-02 19:12:11 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,7 @@ require 'pry'
 class String
 
   def sentence
-    if self.end_with(".") == true
+    if self.end_with?(".") == true
       return true
     else
       return false
@@ -12,7 +12,7 @@ class String
   end
 
   def question?
-    if self.end_with("?") == true
+    if self.end_with?("?") == true
       return true
     else
       return false
@@ -20,7 +20,7 @@ class String
   end
 
   def exclamation?
-    if self.end_with("!") == true
+    if self.end_with?("!") == true
       return true
     else
       return alse
