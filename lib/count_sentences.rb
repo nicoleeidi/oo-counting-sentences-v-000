18cf537a0db2890210a1168c7f9f84c84834682b
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-02 19:18:31 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,41 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".") == true
+      return true
+    else
+      return false
+    end
+
   end
 
   def question?
-
+    if self.end_with?("?") == true
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!") == true
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
+    array= self.split(/[.!?]/)
+    array.count { |x| x != nil && x != "" }
+
+    # array.map do |element|
+    #   if element== nil || ""
+    #     array.delete(element)
+    #   end
+    # end
+    # return array.length
 
   end
+
 end
